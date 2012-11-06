2d552d6e620248cbeb15bf5f68f6dc73bb7dc488
sekdiy <sekdiy@googlemail.com>
2012-11-06 13:18:38 +0100

    erste Implementation der Suche
    - Folgen von '/' werden erkannt
    - Stringoperationen noch offen

--- strip.c
+++ strip.c
@@ -1,11 +1,42 @@
+#include <stdio.h>
 #include <string.h>
 #include "strip.h"
 
-
 char* strip(char* str)
 {
-	// insert your code here
+	char* pos = str;												// Anfangsposition eines ersten '/'
+	char* sub = 0;													// Folgeposition eines nächsten '/'
+
+	printf("DEBUG: durchsuche %s\n\n", str);
 	
+	while (pos != 0) {												// Wiederhole, solange '/' folgen:
+		if ((pos = strchr(pos, '/'))) {								// falls (noch eine) mögliche Folge von '/' ex.,
+			sub = pos;												// dessen Anfangsposition merken
+	
+			printf("DEBUG: erster (lokaler) slash ab %s\n", pos);
+			
+			while (strchr(sub, '/') == sub) {						// falls direkt im Anschluss (eine Folge von) '/' ex.,
+				sub++;												// lokal weitersuchen
+			}
+	
+			printf("DEBUG: letzter (lokaler) slash vor %s\n", sub);
+			
+			if (sub > pos+1) {										// falls mehr als ein '/' hintereinander vorkam...
+				printf("DEBUG: entferne %ld mal '/'\n\n", sub-pos-1);
+	
+				// place string manipulation here...	
+				
+			}
+			else {
+				printf("DEBUG: einzelnes '/', nichts zu tun\n\n");
+				
+				// nothing to be done here...
+			}
+				
+			pos = sub;												// setze fort ab aktueller Position...
+		}
+	}
+			
 	return str;
 }
 
@@ -16,3 +47,4 @@ TestResult test_strip()
 	char* p = strip(str);
 	return (strcmp("/usr/bin/env", p)? NOT_OK : OK);
 }
+
