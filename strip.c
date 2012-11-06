5fba433b8054cf7c4b875ee3bd5f6cca5c084b42
sekdiy <sekdiy@googlemail.com>
2012-11-06 17:30:27 +0100

    erste Abgabeversion
    - iterative Lösung komplettiert
    - strncmp() verwendet (memcpy() wäre eine Alternative)
    - mit etlichen Strings getestet

--- strip.c
+++ strip.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "strip.h"
@@ -5,8 +6,10 @@
 char* strip(char* str)
 {
 	char* pos = str;												// Anfangsposition eines ersten '/'
-	char* sub = 0;													// Folgeposition eines nächsten '/'
-
+	char* sub = NULL;												// Folgeposition eines nächsten '/'
+	
+	char* tmp = NULL;												// Temporärspeicher (für Verschiebungen)
+	
 	printf("DEBUG: durchsuche %s\n\n", str);
 	
 	while (pos != 0) {												// Wiederhole, solange '/' folgen:
@@ -22,29 +25,40 @@ char* strip(char* str)
 			printf("DEBUG: letzter (lokaler) slash vor %s\n", sub);
 			
 			if (sub > pos+1) {										// falls mehr als ein '/' hintereinander vorkam...
-				printf("DEBUG: entferne %ld mal '/'\n\n", sub-pos-1);
+				printf("DEBUG: entferne %ld mal '/'\n", sub-pos-1);
 	
-				// place string manipulation here...	
+				if ((tmp = malloc(strlen(sub))) == (char*) NULL) {	// Platz für den "Suffix" allokieren
+					printf("DEBUG: Fehler beim allokieren von %ld Zeichen. Gebe auf..\n\n", (long) strlen(sub));
+					return NULL;
+				}
 				
+				strncpy(tmp, sub, strlen(sub));						// Substring temporär umkopieren
+				*(tmp + strlen(sub)) = '\0';						// terminieren nicht vergessen...
+				strncpy(pos+1, tmp, strlen(sub));					// temporäre Kopie ans Ziel schreiben
+				*(pos+1 + strlen(sub)) = '\0';						// terminieren nicht vergessen...
+
+				printf("DEBUG: extrahierter Rest ist %s\n", tmp);
+				printf("DEBUG: neuer Zwischenstand wird %s\n\n", str);
+
+				free(tmp);											// allokierten Platz freigeben
+
+				pos++;												// setze fort hinter erstem '/'
 			}
 			else {
+				pos = sub;											// setze fort hinter *dem* '/'
 				printf("DEBUG: einzelnes '/', nichts zu tun\n\n");
-				
-				// nothing to be done here...
-			}
-				
-			pos = sub;												// setze fort ab aktueller Position...
+			}	
 		}
 	}
-			
+	
 	return str;
 }
 
 
 TestResult test_strip()
 {
-	char str[] = "/usr//bin///env";	//example string
+	char str[] = "/usr/////////bin///////////env/tmp///////null/";	//example string
 	char* p = strip(str);
-	return (strcmp("/usr/bin/env", p)? NOT_OK : OK);
-}
 
+	return (strcmp("/usr/bin/env/tmp/null/", p)? NOT_OK : OK);
+}
